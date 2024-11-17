271c1de9c30cd0e49be6f8342a17fc31bdf474b7
tospery <tospery@gmail.com>
2024-11-17 20:54:19 +0800

    button

--- HiSwiftUI/View/ButtonCell.swift
+++ HiSwiftUI/View/ButtonCell.swift
@@ -20,17 +20,45 @@ public struct ButtonCell: View {
     }
     
     public var body: some View {
-        Button(action: action) {
-            HStack {
-                Spacer()
-                Text((model.title ?? "").localizedStringKey)
-                    .font(.system(size: 17))
-                    .foregroundStyle(Color.accentColor)
-                Spacer()
+        Group {
+            if model.style == .plain {
+                Button(action: action) {
+                    HStack {
+                        Spacer()
+                        Text((model.title ?? "").localizedStringKey)
+                            .font(.system(size: 17))
+                            .foregroundStyle(Color.accentColor)
+                        Spacer()
+                    }
+                    .padding(.vertical, 2)
+                }
+                .listRowSeparator(.hidden, edges: .all)
+            } else {
+                HStack(spacing: 0) {
+                    Spacer()
+                        .frame(width: 50)
+                        .onTapGesture {
+                        }
+                    Button(action: action) {
+                        Text((model.title ?? "").localizedStringKey)
+                            .font(.system(size: 17))
+                            .foregroundStyle(Color.background)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 44)
+                            .background(Color.accentColor)
+                            .clipShape(.rect(cornerRadius: 10))
+                    }
+                    .contentShape(Rectangle())
+                    Spacer()
+                        .frame(width: 50)
+                        .onTapGesture {
+                        }
+                }
+                .listRowInsets(.zero)
+                .listRowSeparator(.hidden, edges: .all)
+                .background(Color.green)
             }
-            .padding(.vertical, 2)
         }
-        .listRowSeparator(.hidden, edges: .all)
     }
     
 }
