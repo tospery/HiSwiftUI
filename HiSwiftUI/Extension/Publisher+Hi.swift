44dade2db8a542a42677c9b550c2d74a7eb95356
tospery <tospery@gmail.com>
2024-12-29 05:04:58 +0800

    search

--- HiSwiftUI/Extension/Publisher+Hi.swift
+++ HiSwiftUI/Extension/Publisher+Hi.swift
@@ -13,35 +13,35 @@ private var subscriptions = Set<AnyCancellable>()
 
 public extension Publisher {
     
-    func resultPublisher() -> AnyPublisher<Result<Output, Failure>, Never> {
-        return map(Result.success)
-            .catch { Just(Result.failure($0)) }
-            .eraseToAnyPublisher()
-    }
+//    func resultPublisher() -> AnyPublisher<Result<Output, Failure>, Never> {
+//        return map(Result.success)
+//            .catch { Just(Result.failure($0)) }
+//            .eraseToAnyPublisher()
+//    }
     
-    func async() async -> Result<Output, Failure> where Failure == Error {
-        await withCheckedContinuation { continuation in
-            var cancellable: AnyCancellable?
-            var finishedWithoutValue = true
-            cancellable = first()
-                .sink { result in
-                    switch result {
-                    case .finished:
-                        if finishedWithoutValue {
-                            continuation.resume(returning: .failure(HiError.unknown))
-                        }
-                    case let .failure(error):
-                        continuation.resume(returning: .failure(error))
-                    }
-                    cancellable?.cancel()
-                } receiveValue: { value in
-                    finishedWithoutValue = false
-                    continuation.resume(returning: .success(value))
-                }
-        }
-    }
+//    func async() async -> Result<Output, Failure> where Failure == Error {
+//        await withCheckedContinuation { continuation in
+//            var cancellable: AnyCancellable?
+//            var finishedWithoutValue = true
+//            cancellable = first()
+//                .sink { result in
+//                    switch result {
+//                    case .finished:
+//                        if finishedWithoutValue {
+//                            continuation.resume(returning: .failure(HiError.unknown))
+//                        }
+//                    case let .failure(error):
+//                        continuation.resume(returning: .failure(error))
+//                    }
+//                    cancellable?.cancel()
+//                } receiveValue: { value in
+//                    finishedWithoutValue = false
+//                    continuation.resume(returning: .success(value))
+//                }
+//        }
+//    }
 
-    func asResult() async -> Result<Output, Failure> where Failure == Error {
+    func asResult() async -> Result<Output, Error> {
         do {
             for try await value in self.values {
                 return .success(value)
@@ -63,26 +63,26 @@ public extension Publisher {
         return nil
     }
 
-    func wait(matching targetValue: Output) async -> Output? where Output: Equatable {
-        await withCheckedContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = self
-                .filter { $0 == targetValue }
-                .first()
-                .sink(receiveCompletion: { completion in
-                    switch completion {
-                    case .finished:
-                        continuation.resume(returning: nil)
-                    case .failure:
-                        continuation.resume(returning: nil)
-                    }
-                    cancellable?.cancel()
-                }, receiveValue: { value in
-                    continuation.resume(returning: value)
-                    cancellable = nil
-                })
-        }
-    }
+//    func wait(matching targetValue: Output) async -> Output? where Output: Equatable {
+//        await withCheckedContinuation { continuation in
+//            var cancellable: AnyCancellable?
+//            cancellable = self
+//                .filter { $0 == targetValue }
+//                .first()
+//                .sink(receiveCompletion: { completion in
+//                    switch completion {
+//                    case .finished:
+//                        continuation.resume(returning: nil)
+//                    case .failure:
+//                        continuation.resume(returning: nil)
+//                    }
+//                    cancellable?.cancel()
+//                }, receiveValue: { value in
+//                    continuation.resume(returning: value)
+//                    cancellable = nil
+//                })
+//        }
+//    }
     
 }
 
