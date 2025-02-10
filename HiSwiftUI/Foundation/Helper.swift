e9d0f99a5b53582ac7cbd6159f00eb38b266d4f7
tospery <tospery@gmail.com>
2025-02-10 12:06:19 +0800

    device

--- HiSwiftUI/Foundation/Helper.swift
+++ HiSwiftUI/Foundation/Helper.swift
@@ -15,7 +15,7 @@ public func logEnvironment() {
     log("本地目录: \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.path ?? "")", module: Module.hiSwiftUI)
     log("信息设置: \(profileService.value!)", module: Module.hiSwiftUI)
     log("运行环境: \(UIApplication.shared.inferredEnvironment)", module: Module.hiSwiftUI)
-    log("设备型号: \(UIDevice.current.modelName)", module: Module.hiSwiftUI)
+    log("设备型号: \(UIDevice.current.deviceName)", module: Module.hiSwiftUI)
     log("硬件标识: \(UIDevice.current.uuid)", module: Module.hiSwiftUI)
     log("系统名称: \(UIDevice.current.systemName)", module: Module.hiSwiftUI)
     log("系统版本: \(UIDevice.current.systemVersion)", module: Module.hiSwiftUI)
