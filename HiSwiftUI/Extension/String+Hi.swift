e0d4cc81cf2336505a6d14a03709b443a273fe1d
tospery <tospery@gmail.com>
2026-04-05 21:50:25 +0800

    base

--- HiSwiftUI/Extension/String+Hi.swift
+++ HiSwiftUI/Extension/String+Hi.swift
@@ -28,21 +28,35 @@ public extension String {
     
     var isValidInternalWebUrl: Bool {
         guard isValidWebUrl else { return false }
-        guard self.hasPrefix(UIApplication.shared.baseWebUrl) else { return false }
-        return true
+        guard let components = self.url?.host()?.components(separatedBy: ".") else { return false }
+        if components.count == 2 {
+            return components.first?.lowercased() == Bundle.main.urlScheme()
+        } else if components.count == 3 {
+            return components[1].lowercased() == Bundle.main.urlScheme()
+        }
+        return false
     }
     
-    var isValidUnivLink: Bool {
-        guard isValidWebUrl else { return false }
-        guard self.hasPrefix(UIApplication.shared.baseUnivLink) else { return false }
-        return true
-    }
+//    var isValidUnivLink: Bool {
+//        guard isValidWebUrl else { return false }
+////        guard self.hasPrefix(UIApplication.shared.baseUnivLink) else { return false }
+//        return true
+//    }
     
     var isValidAppUrl: Bool { !isValidWebUrl }
     
     var isValidDeepLink: Bool {
         guard isValidAppUrl else { return false }
-        return self.url?.scheme == UIApplication.shared.urlScheme
+        return self.url?.scheme?.lowercased() == UIApplication.shared.urlScheme
+    }
+    
+    var deepLink: String? {
+        guard isValidInternalWebUrl else { return nil }
+        guard var components = URLComponents(string: self) else { return nil }
+        let scheme = Bundle.main.urlScheme() ?? ""
+        var path = components.path.removingPrefix("/").removingSuffix("/").components(separatedBy: "/").joined(separator: "/")
+        let query = components.percentEncodedQuery.map { "?\($0)" } ?? ""
+        return "\(scheme)://\(path)\(query)"
     }
     
     var routeHost: String {
@@ -50,9 +64,8 @@ public extension String {
             return self.url?.host() ?? ""
         }
         if self.isValidInternalWebUrl {
-            var string = self.removingPrefix(UIApplication.shared.baseWebUrl)
-            string = "\(UIApplication.shared.urlScheme)://\(string)"
-            return string.url?.host() ?? ""
+            guard let components = self.url?.path().removingPrefix("/").removingSuffix("/").components(separatedBy: "/"), components.count != 0 else { return "" }
+            return components.first!
         }
         return ""
     }
@@ -63,10 +76,11 @@ public extension String {
             return path.removingPrefix("/").removingSuffix("/")
         }
         if self.isValidInternalWebUrl {
-            var string = self.removingPrefix(UIApplication.shared.baseWebUrl)
-            string = "\(UIApplication.shared.urlScheme)://\(string)"
-            guard let path = string.url?.path() else { return "" }
-            return path.removingPrefix("/").removingSuffix("/")
+            guard let components = self.url?.path().removingPrefix("/").removingSuffix("/").components(separatedBy: "/") else { return "" }
+            if components.count <= 1 {
+                return ""
+            }
+            return components.dropFirst().joined(separator: "/")
         }
         return ""
     }
